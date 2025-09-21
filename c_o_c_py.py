eb56a4874654acf7c3b41523157ac410b7379dbb
twattus <jakobakhtar2009@outlook.com>
2025-09-21 21:32:42 +0100

    Update to c_o_c_py.py, it's the new version now

--- c_o_c_py.py
+++ c_o_c_py.py
@@ -9,10 +9,22 @@ pygame.init()
 screen=pygame.display.set_mode((screen_x,screen_y))
 pygame.display.set_caption('C_o_c testing')
 
+
+transparent_p1_cooldown_bg=pygame.Surface((200,screen_y))
+transparent_p1_cooldown_bg.set_alpha(96)
+transparent_p1_cooldown_bg.fill((255,192,129))
+
+transparent_p2_cooldown_bg=pygame.Surface((200,screen_y))
+transparent_p2_cooldown_bg.set_alpha(96)
+transparent_p2_cooldown_bg.fill((192,255,192))
+
 p1_points_bar=0.0
 p2_points_bar=0.0
+p1_regen_speed=1.0
+p2_regen_speed=1.0
 
-#2d list, [hp,speed,atk_speed,range,behaviour_id,attack_id,size,cost]
+p1_deck_options=[1,2,3,4,5,6,7,8] #legend has it that two of the same card cannot be found on the same options table
+p2_deck_options=[1,2,3,4,5,6,7,8] #Hugo is a reasonably reliable source 
 
 #ids are as follows
 bg_img=pygame.image.load("background_area_piskel.png").convert_alpha()
@@ -20,24 +32,38 @@ bg_img=pygame.image.load("background_area_piskel.png").convert_alpha()
 red_victory_png=pygame.image.load("red_victory_piskel.png").convert_alpha()
 green_victory_png=pygame.image.load("green_victory_piskel.png").convert_alpha()
 
-tile_images=[0,
+tile_images=[0, #the tower has no icon because it can't be deployed
              pygame.image.load("generic_fellow_piskel.png").convert_alpha(),
              pygame.image.load("giant_piskel.png").convert_alpha(),
-             pygame.image.load("speedy_boi_piskel.png").convert_alpha(),]
+             pygame.image.load("speedy_boi_piskel.png").convert_alpha(),
+             pygame.image.load("wizard_icon_piskel.png").convert_alpha(),
+             pygame.image.load("angry_tree_fellow_piskel.png").convert_alpha(),
+             pygame.image.load("distraction_bot_piskel.png").convert_alpha(),
+             pygame.image.load("archer_piskel.png").convert_alpha(),
+             pygame.image.load("regen_piskel.png").convert_alpha(),]
 
+#2d list, [hp,speed,atk_speed,range,behaviour_id,attack_id,size,cost]
 unit_data=[[1000,0,120,300,0,0,64,-1], #0:tower
-           [40,1,60,32,1,0,24,2],#1:generic fellow
-           [500,0.4,120,100,2,3,36,4], #2:big boi
-           [50,2,6,128,1,2,20,3], #3: speedy fellow
-           [],
+           [100,1,60,32,1,0,24,2],#1:generic fellow
+           [300,0.4,120,100,2,3,36,4], #2:big boi
+           [100,2,6,128,1,2,20,3], #3: speedy fellow
+           [200,0.8,15,150,1,4,40,5], #4: wizard
+           [500,0.1,360,300,1,5,48,5], #5: ent
+           [400,2,1200,100,1,6,16,1], #6: distractor
+           [400,1.5,20,250,1,7,24,2], #7: archer
+           [1,1,1,1,1,1,1,7], #8: regen upgrade
            []
            ] 
 
 #2d list: [dmg,abs_speed,size]
 projectile_data=[[10,40,16], #0:fire or smthn idk
                  [20,10,18], #1:strong proj idk 
-                 [3,20,8], #2:bullet ig
+                 [15,20,8], #2:bullet ig
                  [50,5,40], #3: big boi attack (very technical vocabulary)
+                 [40,10,16], #4: magic thing ig
+                 [200,5,60], #5: tree?
+                 [20,20,32], #6: laser
+                 [16,10,20], #7: arrow
                  []]
 
 class projectile:
@@ -239,8 +265,8 @@ class container:
                 if pygame.Rect.colliderect(self.projectiles[e].rect,self.units[f].rect):
                     self.units[f].hp-=self.projectiles[e].dmg
                     if self.units[f].hp<=0:
-                        self.units[f].x-=10000 #same as with proj, its just safer to do it this way
-                    self.projectiles[e].x-=10000 #casts the projectile into the void, because removing items during iteration is severely dangerous
+                        self.units[f].x-=10000000 #same as with proj, its just safer to do it this way
+                    self.projectiles[e].x-=10000000 #casts the projectile into the void, because removing items during iteration is severely dangerous
                     break
         self.validate()
 
@@ -275,16 +301,43 @@ units_container.units.extend([unit(500-32,64,0,False),unit(300-32,96,0,False),un
 #     units_container.units.append(unit(400+random.randint(-100,100),[700,300][e%2],[1,1,1,2][e%4],[True,False][e%2])) #debug testing
 
 p1_options=[]
-p1_cooldown=30
+p1_cooldown=0
 p2_options=[]
-p2_cooldown=30
+p2_cooldown=0
 
 def draw_options(options_list,x):
     for e in range(0,len(options_list)):
         screen.blit(tile_images[options_list[e]],(x,(16*math.sin((tick/160)+20*e))+100+(180*e)))
         #pygame.draw.rect(screen,[(0,0,0),(255,128,32),(128,255,32),(32,128,255)][options_list[e]],pygame.Rect(x,(16*math.sin((tick/160)+20*e))+100+(180*e),100,128))
 
+
+def refill_p1_options(removal_index):
+        global p1_options,p1_deck_options
+        while len(p1_options)<4:
+            if len(p1_deck_options)>=5:
+                random.shuffle(p1_deck_options)
+                if p1_deck_options[0] not in p1_options:
+                    p1_options.insert(removal_index,p1_deck_options[0])
+                    p1_deck_options.pop(0)
+            else:
+                p1_deck_options=[1,2,3,4,5,6,7,8]
+
+def refill_p2_options(removal_index):
+        global p2_options,p2_deck_options
+        while len(p2_options)<4:
+            if len(p2_deck_options)>=5:
+                random.shuffle(p2_deck_options)
+                if p2_deck_options[0] not in p2_options:
+                    p2_options.insert(removal_index,p2_deck_options[0])
+                    p2_deck_options.pop(0)
+            else:
+                p2_deck_options=[1,2,3,4,5,6,7,8]
+
+refill_p1_options(0)
+refill_p2_options(0)
+
 def choose_options():
+    global p1_regen_speed,p2_regen_speed
     global p1_cooldown,p1_points_bar
     if p1_cooldown==0 and (board[pygame.K_1] or board[pygame.K_2] or board[pygame.K_3] or board[pygame.K_4]):
         temp_p1_choices=[board[pygame.K_1],board[pygame.K_2],board[pygame.K_3],board[pygame.K_4]]
@@ -292,9 +345,14 @@ def choose_options():
             if temp_p1_choices[e]:
                 if p1_points_bar>=unit_data[p1_options[e]][7]:
                     p1_points_bar-=unit_data[p1_options[e]][7]
-                    p1_cooldown=30
-                    units_container.units.append(unit(random.randint(-40,40)+(screen_x/2),200,p1_options[e],False))
-                    p1_options[e]=random.choice([1,1,1,2,3,3])
+                    # print(unit_data[p1_options[e]],unit_data[p1_options[e]][7])
+                    p1_cooldown=60
+                    if p1_options[e]==8:
+                        p1_regen_speed+=1
+                    else:
+                        units_container.units.append(unit(random.randint(-40,40)+(screen_x/2),200,p1_options[e],False))
+                    p1_options.pop(e)
+                    refill_p1_options(e)
                     break
 
     global p2_cooldown,p2_points_bar
@@ -304,9 +362,13 @@ def choose_options():
             if temp_p2_choices[e]:
                 if p2_points_bar>=unit_data[p2_options[e]][7]:
                     p2_points_bar-=unit_data[p2_options[e]][7]
-                    p2_cooldown=30
-                    units_container.units.append(unit(random.randint(-40,40)+(screen_x/2),screen_y-200,p2_options[e],True))
-                    p2_options[e]=random.choice([1,1,2,3,3])
+                    p2_cooldown=60
+                    if p2_options[e]==8:
+                        p2_regen_speed+=1
+                    else:
+                        units_container.units.append(unit(random.randint(-40,40)+(screen_x/2),screen_y-200,p2_options[e],True))
+                    p2_options.pop(e)
+                    refill_p2_options(e)
                     break
 
 def player_presence():
@@ -342,25 +404,45 @@ while True:
                                         unit(500-32,screen_y-64,0,True),unit(300-32,screen_y-96,0,True),unit(700-32,screen_y-96,0,True)])
             p1_cooldown=0
             p1_options=[]
-            p1_points_bar=0.
-            
+            p1_points_bar=0.0
+            p1_regen_speed=1
+            p1_deck_options=[1,2,3,4,5,6,7,8]
+            refill_p1_options(0)
+
+
             p2_cooldown=0
             p2_options=[]
             p2_points_bar=0.0
+            p2_regen_speed=1   
+            p2_deck_options=[1,2,3,4,5,6,7,8] 
+            refill_p2_options(0)
+
     else:
-        p1_points_bar+=0.01
-        p2_points_bar+=0.01
+        p1_points_bar+=0.01*p1_regen_speed
+        p2_points_bar+=0.01*p2_regen_speed
         p1_points_bar=min(p1_points_bar,10) #why not, let spaghetti code take the wheel
         p2_points_bar=min(p2_points_bar,10)
         p1_cooldown=max(p1_cooldown-1,0)
         p2_cooldown=max(p2_cooldown-1,0)
 
         choose_options()
-        while len(p1_options)<4:
-            p1_options.append(random.choice([1,1,2,3,3]))
-        while len(p2_options)<4:
-            p2_options.append(random.choice([1,1,2,3,3]))
-
+        # while len(p1_options)<4:
+        #     if len(p1_deck_options)>=1:
+        #         random.shuffle(p1_deck_options)
+        #         if p1_deck_options[0] not in p1_options:
+        #             p1_options.append(p1_deck_options[0])
+        #             p1_deck_options.pop(0)
+        #     else:
+        #         p1_deck_options=[1,2,3,4,5,6,7,8]
+        # while len(p2_options)<4:
+        #     if len(p2_deck_options)>=1:
+        #         random.shuffle(p2_deck_options)
+        #         if p2_deck_options[0] not in p2_options:
+        #             p2_options.append(p2_deck_options[0])
+        #             p2_deck_options.pop(0)
+        #     else:
+        #         p2_deck_options=[1,2,3,4,5,6,7,8]
+            
         
 
         #<DRAW ZONE>
@@ -386,6 +468,10 @@ while True:
         pygame.draw.rect(screen,(0,0,0),pygame.Rect(screen_x-190,32,190,4))
         draw_options(p1_options,50)
         draw_options(p2_options,850)
+        if p1_cooldown:
+            screen.blit(transparent_p1_cooldown_bg,(0,36)) 
+        if p2_cooldown:
+            screen.blit(transparent_p2_cooldown_bg,(800,36)) 
 
         #print(player_presence())
         units_container.update()
